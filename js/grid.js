d2c55e92e7fd09922311238d1c029437e499b15f
Icefox <icefox.990@gmail.com>
2015-09-07 13:22:10 -0300

    TODO: fix grid remaking

--- js/grid.js
+++ js/grid.js
@@ -1,19 +1,33 @@
-var rows = 16;
-var columns = 16;
+var squares = 4;
+var winSize = 640;
 var $row = $("<div />", {class: "row"});
 var $square = $("<div />", {class: "square"});
 
 $(document).ready(function(){
-	for (var i = 0; i < columns; i++) {
-		$row.append($square.clone());
-	}
-	for (var i = 0; i < rows; i++) {
-		$("#container").append($row.clone());
-	}
+	remakeGrid(squares);
 
 	$(".square").hover(function() {
 		var randomColor = Math.floor(Math.random()*16777215).toString(16);
 		$(this).css("background", "#" + randomColor);
 	});
+
+	$(".blocks").click(function() {
+		squares = prompt("What will the new grid X size be? (X * X)");
+		remakeGrid(squares);
+	});
 });
 
+function remakeGrid(newS){
+	$row.css("height", winSize / newS);
+	$square.css({
+		'height': winSize / newS,
+		'width': winSize / newS
+	});
+
+	for (var i = 0; i < newS; i++) {
+		$row.append($square.clone());
+	}
+	for (var i = 0; i < newS; i++) {
+		$("#container").append($row.clone());
+	}
+}
