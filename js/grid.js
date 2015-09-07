61478d00fe78a8de733da2098e76e3a15ca2d93f
Icefox <icefox.990@gmail.com>
2015-09-07 11:57:27 -0300

    updated html/css

---
+++ js/grid.js
@@ -0,0 +1,13 @@
+var rows = 16;
+var columns = 16;
+var $row = $("<div />", {class: "row"});
+var $square = $("<div />", {class: "square"});
+
+$(document).ready(function(){
+	for (var i = 0; i < columns; i++) {
+		$row.append($square.clone());
+	}
+	for (var i = 0; i < rows; i++) {
+		$("wrapper").append($row.clone());
+	}
+});
